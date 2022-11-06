701b0e3a336e9f8ede769429c78e55a11eb7ed77
Soobin Kim <soobin.kim@fruitsfamily.com>
2022-11-07 06:17:56 +0900

    [1107] how to use 'as' and 'attrs' in styled-components

--- src/App.js
+++ src/App.js
@@ -2,22 +2,29 @@ import styled from "styled-components";
 
 const Father = styled.div`
   display: flex;
-`
-const Box = styled.div`
-  background-color: ${props => props.bgColor ?? 'blue'};
-  width: 100px;
-  height: 100px;
-`
-const Circle = styled(Box)`
-  border-radius: 50px;
+`;
+const Btn = styled.button`
+  color: white;
+  background-color: tomato;
+  border: 0;
+  border-radius: 15px;
+`;
+const Input = styled.input.attrs({required: true})`
+  background-color: tomato;
 `
 
 function App() {
   return (
-    <Father>
-      <Box bgColor={"tomato"}/>
-      <Box bgColor={"teal"}/>
-      <Circle/>
+    <Father as="header">
+      <Input/>
+      <Input/>
+      <Input/>
+      <Input/>
+      <Input/>
+      <Btn>Log in</Btn>
+      <Btn as="a" href="/">
+        Log in
+      </Btn>
     </Father>
   );
 }
